1aba7b5394ef941b9d9d045647a9df00f2b3b0ff
PamothKumarasinghe <nikolailaazz2@gmail.com>
2025-09-11 18:50:18 +0530

    Create auth.js

---
+++ auth-backend/routes/auth.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { signup, login, getProfile, updatePassword, deleteUser } from '../controllers/authController.js';
+
+const router = express.Router();
+
+router.post("/signup", signup); // when server receives a post request at /signup,
+                                // it will call the signup function from controller
+router.post("/login", login);   // when server receives a post request at /login,
+                                // it will call the login function from controller
+router.get("/profile", authMiddleware, getProfile); // when server receives a get request at /profile,
+
+router.put("/update-password", authMiddleware, updatePassword);
+
+router.delete("/delete", authMiddleware, deleteUser);
+
+export default router;
